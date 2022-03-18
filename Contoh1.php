6a4f6cfd77630f1ad525d39983d90c6d96a67b50
Paganiniani <95736700+9844663@users.noreply.github.com>
2022-03-18 19:27:33 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+   public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "<h4>Nama saya Adellina Sylvira Azis</h4>";
+        echo "Saya berasal dari Universitas Bina Sarana Informatika Margonda";
+    }
+    
+}
